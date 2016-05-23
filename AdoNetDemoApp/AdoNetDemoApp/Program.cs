f43440aa8ec347a53611ae33996d198b673169ba
Maksim_Soldatenko <Maksim_Soldatenko@epam.com>
2016-05-23 17:34:27 +0300

    Simple Insert and Select

--- AdoNetDemoApp/AdoNetDemoApp/Program.cs
+++ AdoNetDemoApp/AdoNetDemoApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,29 @@ namespace AdoNetDemoApp
     {
         static void Main(string[] args)
         {
+            var cs = ConfigurationManager.ConnectionStrings["DemoSchoolContext"].ConnectionString;
 
+            using (var connection = new SqlConnection(cs))
+            {
+                connection.Open();
+                var command = new SqlCommand("Insert into People values ('Vasyl', 'Gupalo',null,'10.25.2015','Student')", connection);
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+
+                command.CommandText = "Select * from People";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} - {1} - {2}", reader[0], reader[1], reader[2]));
+                    }
+                }
+
+                connection.Close();
+            }
+
+            Console.ReadKey();
         }
     }
 }
