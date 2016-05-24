36d0c2ecb8b5c3758ed59983d6fe34cb58e54966
maksim-soldatenko <maksim.soldatenko@gmail.com>
2016-05-25 01:08:23 +0300

    Transient error handling

--- AdoNetDemoApp/AdoNetDemoApp/Program.cs
+++ AdoNetDemoApp/AdoNetDemoApp/Program.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
 
 namespace AdoNetDemoApp
 {
@@ -14,6 +18,8 @@ namespace AdoNetDemoApp
         static void Main(string[] args)
         {
             var cs = ConfigurationManager.ConnectionStrings["DemoSchoolContext"].ConnectionString;
+
+            GetWithRetry(cs);
 
             using (var connection = new SqlConnection(cs))
             {
@@ -63,6 +69,45 @@ namespace AdoNetDemoApp
                 }
 
                 connection.Close();
+            }
+        }
+
+        private static void GetWithRetry(string connectionString)
+        {
+            var retryStrategy = new ExponentialBackoff("exp1", 5, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(40),
+                TimeSpan.FromSeconds(1));
+
+            var sts = new List<RetryStrategy>() {retryStrategy};
+            var manager = new RetryManager(sts, "exp1");
+            RetryManager.SetDefault(manager, false);
+
+            //ITransientErrorDetectionStrategy
+            //SqlAzureTransientErrorDetectionStrategy
+            var retryPolicy = new RetryPolicy<SqlDatabaseTransientErrorDetectionStrategy>(retryStrategy);
+
+            retryPolicy.Retrying += (sender, eventArgs) =>
+            {
+                Debugger.Launch();
+                Console.WriteLine("Retrying, CurrentRetryCount = {0} , Exception = {1}", eventArgs.CurrentRetryCount, eventArgs.LastException.Message);
+            };
+
+            using (var reliableConnection = new ReliableSqlConnection(connectionString, retryPolicy, retryPolicy))
+            {
+                using (var connection = reliableConnection.Open())
+                {
+                    var command = new SqlCommand("SELECT * FROM People WHERE Discriminator = 'Student'", connection);
+
+                    retryPolicy.ExecuteAction(() =>
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(String.Format("{0} - {1} - {2} - {3}", reader[0], reader[1], reader[2], reader["Discriminator"]));
+                            }
+                        }
+                    });
+                }
             }
         }
     }
