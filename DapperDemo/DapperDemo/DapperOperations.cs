bf3f72a3569e39407d60397783a88543c3a1933e
Maksim_Soldatenko <Maksim_Soldatenko@epam.com>
2016-05-24 19:13:32 +0300

    Dapper Demo Project

---
+++ DapperDemo/DapperDemo/DapperOperations.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperDemo.DomainModel;
+
+namespace DapperDemo
+{
+    public class DapperOperations
+    {
+        private readonly string _cs;
+
+        public DapperOperations()
+        {
+            _cs = ConfigurationManager.ConnectionStrings["DemoSchoolContext"].ConnectionString;
+        }
+
+        public void Create()
+        {
+            var student = new Student() {EnrollmentDate = DateTime.Now, FirstMidName = "Gupalo", LastName = "Vasyl"};
+
+            using (var connection = new SqlConnection(_cs))
+            {
+                var students =
+                    connection.Execute(
+                        "INSERT INTO People (LastName, FirstMidName, EnrollmentDate, Discriminator) values (@lastName, @firstMidName, @enrollmentDate, 'Student')",
+                        new
+                        {
+                            lastName = student.LastName,
+                            firstMidName = student.FirstMidName,
+                            enrollmentDate = student.EnrollmentDate
+                        });
+
+                Console.WriteLine("Added {0}", students);
+            }
+        }
+
+        public void Read()
+        {
+            ReadAll();
+            ReadFiltered("Li");
+        }
+
+        public void Update(string lastName)
+        {
+            using (var connection = new SqlConnection(_cs))
+            {
+                var student =
+                    connection.Query<Student>(
+                        "SELECT * FROM PEOPLE WHERE Discriminator = 'Student' AND LastName LIKE @lastName",
+                        new {lastName = String.Format("%{0}%", lastName)}).FirstOrDefault();
+
+                if (student != null)
+                {
+                    student.EnrollmentDate = DateTime.Now;
+
+                    var upd = connection.Execute("UPDATE People SET EnrollmentDate = @enrollmentDate WHERE Id = @id",
+                        new {enrollmentDate = student.EnrollmentDate, id = student.Id});
+
+                    Console.WriteLine("Updated {0}", upd);
+
+                }
+            }
+        }
+
+        public void Delete(string lastName)
+        {
+            using (var connection = new SqlConnection(_cs))
+            {
+                var students =
+                    connection.Execute(
+                        "DELETE FROM PEOPLE WHERE Discriminator = 'Student' AND LastName LIKE @lastName",
+                        new {lastName = String.Format("%{0}%", lastName)});
+
+                Console.WriteLine("Deleted {0}", students);
+            }
+        }
+
+        private void ReadFiltered(string lastName)
+        {
+            using (var connection = new SqlConnection(_cs))
+            {
+                var students =
+                    connection.Query<Student>(
+                        "SELECT * FROM PEOPLE WHERE Discriminator = 'Student' AND LastName LIKE @lastName",
+                        new {lastName = String.Format("%{0}%", lastName)});
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine(String.Format("{0} - {1} - {2}", student.Id, student.FirstMidName, student.LastName));
+                }
+            }
+        }
+
+        private void ReadAll()
+        {
+            using (var connection = new SqlConnection(_cs))
+            {
+                var students = connection.Query<Student>("SELECT * FROM PEOPLE WHERE Discriminator = 'Student'");
+                foreach (var student in students)
+                {
+                    Console.WriteLine(String.Format("{0} - {1} - {2}", student.Id, student.FirstMidName, student.LastName));
+                }
+            }
+        }
+    }
+}
---
+++ DapperDemo/DapperDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dpr = new DapperOperations();
+
+            var run = true;
+
+            while (run)
+            {
+                Console.WriteLine("What operation (CRUD) do you want to perform?");
+                var op = Console.ReadLine();
+
+                switch (op.ToUpper())
+                {
+                    case "C":
+                        dpr.Create();
+                        break;
+                    case "R":
+                        dpr.Read();
+                        break;
+                    case "U":
+                        dpr.Update("vasyl");
+                        break;
+                    case "D":
+                        dpr.Delete("vasyl");
+                        break;
+                    case "E":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Undefined operation");
+                        break;
+                }
+            }
+        }
+    }
+}
